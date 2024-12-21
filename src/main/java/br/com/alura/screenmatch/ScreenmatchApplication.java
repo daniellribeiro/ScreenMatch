8c6f8dd9c4a5018b107bb3bc9889640f98346cfc
Daniel Ribeiro <daniellopes03@hotmail.com>
2024-12-21 15:54:31 -0300

    Commit

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,22 @@
 package br.com.alura.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.alura.screenmatch.service.*;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoAPI consumoAPI = new ConsumoAPI();
+		String json = consumoAPI.obterDados("https://www.omdbapi.com/?t=The+Big+Bang+Theory&Season=2&apikey=687abcb1");
+		System.out.println(json);
+
+	}
 }
